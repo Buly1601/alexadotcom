8c6d7fef95ff1521fb67c3f7ea5168736c5525ff
Charbel Breydy Torres <66335475+Buly1601@users.noreply.github.com>
2021-09-18 23:38:31 -0500

    first db approach

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,27 @@
 import os
 import re
 from flask import Flask, render_template, request
-#from werkzeug.security import generate_password_hash, check_password_hash
-#from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 #from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+# setting up db
+# app.config["SQALCHEMY_DATABASE_URI"] = "sqlite:///registration.db"
+db = SQLAlchemy(app)
+
+# basic registration
+class User_password(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    name = db.Column(db.Text)
+    password = db.Column(db.String)
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
 
 with app.app_context():
     
@@ -29,17 +45,22 @@ with app.app_context():
             conf_passoword = request.form.get("conf_psswd")
             error = None
 
+            # check for potential errors before adding to db
             if not username:
-                error = "Username is required"
+                error = "Username is required."
             elif not password:
-                error = "Password is required"
+                error = "Password is required."
             elif password != conf_passoword:
-                error = "Passwords must match"
-            # TODO check existance of user in db
+                error = "Passwords must match."
+            elif User_password.query.filter_by(name=username).first() is not None:
+                error = f"User {username} already exists, try logging in."
 
             if not error:
                 # TODO need JSON
-                return "success"
+                new_user = User_password(username, generate_password_hash(password))
+                db.session.add(new_user)
+                db.session.commit()
+                return "Success"
 
             else:
                 return error, 402
@@ -57,22 +78,34 @@ with app.app_context():
             # take information from post
             username = request.form.get("username")
             password = request.form.get("password")
+            user_check = User_password.query.filter_by(name=username).first()
             error = None
 
             if not username:
                 error = "Username is required"
             elif not password:
                 error = "Password is required"
-            # TODO check the existance of user and password with hash
+            elif user_check is None:
+                error = "Nonexistent username"
+            elif check_password_hash(user_check.password, password):
+                error = "Incorrect password"
 
             if not error:
                 # TODO return json
-                return "success"
+                # TODO render user's template
+                return "success", 200
             else:
                 return error,402
         
         else:
             return render_template("register_page.html", title="Register", url=os.getenv("URL"))
+    
+    @app.route("/__health__")
+    def healthy():
+        """
+        Health monitoring help function
+        """
+        return ""
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="5000", debug=True)
